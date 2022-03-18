76da7a09935073d14d33b5f15eff8f4af15f2e9a
iSTeeWx <vdsti2006@gmail.com>
2022-03-19 00:10:07 +0100

    Jeu pions

--- Jeu pions/main.py
+++ Jeu pions/main.py
@@ -5,7 +5,6 @@ def main():
     player: chr = '0'
 
     while playing:
-        print_list(board)
         player = ['0', 'X'][player == '0']
 
         valid_start: bool = False
@@ -16,12 +15,15 @@ def main():
         to_x: int = -1
         to_y: int = -1
 
+        print_list(board)
+        print('Turn of', player)
+
         while not valid_start:
             while from_x == -1:
                 temp: str = input('From X: ')
 
                 if test_input(temp):
-                    from_x = int(temp)
+                    from_x = int(temp) - 1
                 else:
                     error('Not a valid number!\nShould be between but not including 0-9')
 
@@ -29,11 +31,11 @@ def main():
                 temp: str = input('From Y: ')
 
                 if test_input(temp):
-                    from_y = int(temp)
+                    from_y = int(temp) - 1
                 else:
                     error('Not a valid number!\nShould be between but not including 0-9')
 
-            if board[from_y - 1][to_y - 1] != player:
+            if board[from_y][from_x] != player:
                 error('You don\'t have a piece in this space')
                 from_x = -1
                 from_y = -1
@@ -41,29 +43,45 @@ def main():
             else:
                 valid_start = True
 
-        while to_x == -1:
-            temp: str = input('To X: ')
+        while not valid_destination:
+            while to_x == -1:
+                temp: str = input('To X: ')
 
-            if test_input(temp):
-                if int(temp) != from_x:
-                    to_x = int(temp)
+                if test_input(temp):
+                    to_x = int(temp) - 1
                 else:
-                    error('Not a valid number!\nShould be different from starting x')
+                    error('Not a valid number!\nShould be between but not including 0-9')
 
-            else:
-                error('Not a valid number!\nShould be between but not including 0-9')
+            while to_y == -1:
+                temp: str = input('To Y: ')
 
-        while to_y == -1:
-            temp: str = input('To Y: ')
-
-            if test_input(temp):
-                if int(temp) != from_y:
-                    to_y = int(temp)
+                if test_input(temp):
+                    to_y = int(temp) - 1
                 else:
-                    error('Not a valid number!\nShould be different from starting y')
+                    error('Not a valid number!\nShould be between but not including 0-9')
 
+            if from_x == to_x and from_y == to_y:
+                error('Not a valid number!\nShould be different from starting position')
+                to_x = -1
+                to_y = -1
+                continue
+
+            if abs(to_x - from_x) > 1 or abs(to_y - from_y) > 1:
+                error('Not a valid number!\nDestionation is to far')
+                to_x = -1
+                to_y = -1
+                continue
+
+            if board[to_y][to_x] == player:
+                error('Space already occupied by self')
+                to_x = -1
+                to_y = -1
+                valid_destination = False
             else:
-                error('Not a valid number!\nShould be between but not including 0-9')
+                valid_destination = True
+
+        board[from_y][from_x] = '▯'
+        board[to_y][to_x] = player
 
 
 def test_input(str_in: str) -> bool:
@@ -80,7 +98,7 @@ def create_board() -> list:
     width: int = 8
     height: int = 8
 
-    out = [[' ' for _ in range(width)] for __ in range(height)]
+    out = [['▯' for _ in range(width)] for __ in range(height)]
 
     for i in range(width):
         # Player that sits at the top of the grid
